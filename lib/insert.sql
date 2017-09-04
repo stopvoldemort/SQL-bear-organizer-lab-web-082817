364d175c082e0d64e45ce97709fafcab50ec414e
stopvoldemort <david.squires1@gmail.com>
2017-09-04 12:44:18 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, "M", "brown", NULL, 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 25, "F", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 14, "M", "black", "hungry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 13, "M", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 11, "F", "brown", "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, "F", NULL, "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, "M", NULL, "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "brown", "hungry", 0);
